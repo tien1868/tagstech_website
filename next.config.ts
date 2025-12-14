ab03b8cc5325e162ea042f6dddbd006c2b63f927
tien1868 <jacksorob@gmail.com>
2025-12-14 10:53:47 -0700

    Enable unoptimized images and ESLint ignore

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
